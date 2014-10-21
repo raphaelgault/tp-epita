b21858efa932efa613c734a46f36509d7315a6ae
Dakey <raph.gmail@gmail.com>
2014-10-21 20:44:33 +0200

    1.3

--- S1_TP5/tp5.ml
+++ S1_TP5/tp5.ml
@@ -7,6 +7,24 @@ let append l1 l2 =
   in 
   aux l1;;
 
+let rec already_in a = function
+  |[] -> false
+  |h::t when h = a -> true
+  |h::t -> already_in a t;;
+
+let  my_list_length l =
+  let rec aux acc = function
+    |[] -> acc
+    |h::t -> aux (acc+1) t
+  in
+  aux 0 l;;
+
+let rec my_power x = function
+    |0 -> 1
+    |n when n mod 2 = 0-> my_power (x*x) (n/2) 
+    |n -> x * my_power (x*x) (n/2);;
+
+
 (*==========================================
             STAGE 00 - Must do
 ==========================================*)
@@ -65,7 +83,7 @@ type boolean =
 (* 1.1 Value ? *)
 
 let rec value s = function
-  |[] -> failwith "unbound value"
+  |[] -> failwith ("Unbound Var :"^s)
   |(a,bundle)::t -> 
     if a = s then
      bundle
@@ -73,11 +91,15 @@ let rec value s = function
 
 (* 1.2 Extraction *)
 
-let rec extract = function
-  |True | False -> []
-  |Var s -> [s]
-  |Not b -> extract b
-  |And (b1,b2) | Or (b1,b2) -> append (extract b1) (extract b2);;
+(* using the function already_in defined at the top of the file *)
+let extract b =
+  let rec aux l = function
+    |True | False -> l 
+    |Var s -> if (already_in s l) then l else s::l
+    |Not b -> aux l b
+    |And (b1,b2) | Or (b1,b2) -> aux (aux l b1) b2
+  in
+  aux [] b;;
 
 (* 1.3 Generate *)
 
@@ -86,3 +108,35 @@ let rec generate = function
   |[] ->[]
   |h::t -> (h,True)::(h,False)::(generate t);;
 i*)
+
+(*step 1 : count number of given identifiers -> my_list_length *)
+(*step 2 : compute number of combination -> power of 2 *)
+(*step 3 : fix the first id for 2^(n-1) combination and the second id for
+ * 2^(n-2) ... n id not fixed -> move at every combination *)
+(*step 4 : each combination is stored inside a list which is in a list too *)
+
+let rec distrib a = function
+  |[] -> []
+  |h::t -> (a::h)::distrib a t
+
+let rec generate = function
+  |[] -> []
+  |h::[] -> [(h,True)]::[(h,False)]::[]
+  |h1::t -> distrib (h1,True) (generate t)@
+    distrib (h1,False) (generate t);;
+
+
+
+(*
+let rec generate = function
+    |[] -> []
+    |h::[] -> combine h
+    |h1::h2::t ->
+      begin 
+        match (combine h1,combine h2) with 
+          |(a1::a2::[],b1::b2::[]) -> [b1::a1]::[b1::[a2]]::[b2::[a1]]::[b2::[a2]]::[]
+          |_ -> failwith "booh" 
+      end;;
+     
+
+*)
